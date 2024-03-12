642cfe5f507021a83292cd3c548d41dd2342a93f
Zwaneem <zwaneem68@outlook.com>
2024-03-12 21:32:03 +0200

    all

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-let currentBase = 0;
-
 exports.converter = function (base) {
-  currentBase = base;
   return function (number) {
-    return (number.toString(currentBase));
+    return (number.toString(base));
   };
 };
