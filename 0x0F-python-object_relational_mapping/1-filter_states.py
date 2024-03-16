5e7adfb3b72b4018f71ae25162d916be0ea61482
ZwaneEM <zwaneem68@outlook.com>
2024-03-16 08:46:48 +0200

    all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,33 +3,36 @@
 from sys import argv
 import MySQLdb
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
-"""
-connecting to MySQL
-"""
+    """
+    connecting to MySQL
+    """
 
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database
-    )
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-"""
-executing queries
-"""
+    """
+    executing queries
+    """
 
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE \
+            name LIKE 'N%' ORDER BY id ASC")
 
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
