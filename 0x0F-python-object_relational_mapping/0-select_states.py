f6dc232bad9df53b503401815d96412bbc5fbd85
ZwaneEM <zwaneem68@outlook.com>
2024-03-17 13:34:19 +0200

    all

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
+    """ bash arguments """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+    """ creation of cursor """
+
+    cursor = conn.cursor()
+
+    sql_query = "SELECT * FROM states"
+    cursor.execute(sql_query)
+
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
+
+    cursor.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N) from
-the database hbtn_0e_0_usa"""
+
+"""
+list all states with a name starting with 'N'
+"""
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
+    """ command line arguments """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = conn.cursor()
+
+    sql_query = "SELECT * FROM states WHERE states.name LIKE 'N%'"
+    cursor.execute(sql_query)
+
+    rows = cursor.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    cur.close()
-    db.close()
+        print(row)
+
+    cursor.close()
+    conn.close()
