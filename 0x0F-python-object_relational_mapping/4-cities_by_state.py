e7f96cbc2fe0b814a5bf01b3a1108bc024c62851
ZwaneEM <zwaneem68@outlook.com>
2024-03-17 14:36:56 +0200

    all

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+
+"""
+lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ command line arguments """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    """connect to the database """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    """ creating a cursor """
+    cursor = conn.cursor()
+
+    sql_query = ("SELECT cities.id, cities.name, states.name \
+    FROM cities INNER JOIN\
+     states ON states.id = cities.state_id ORDER BY cities.id ASC")
+
+    cursor.execute(sql_query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
