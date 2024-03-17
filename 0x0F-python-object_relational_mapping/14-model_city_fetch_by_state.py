5c0451a13bad2738857977fd7310e5d70110e96b
ZwaneEM <zwaneem68@outlook.com>
2024-03-18 00:05:27 +0200

    all

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+prints all city objects from the database
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(State, City).\
+        join(City, State.id == City.state_id).order_by(City.id).all()
+
+    for state, city in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Definition of a city
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship('State')
