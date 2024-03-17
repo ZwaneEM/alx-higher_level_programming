21eed01857e0a58360b0ca44ce48b24342e20132
ZwaneEM <zwaneem68@outlook.com>
2024-03-18 00:09:00 +0200

    all

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}".
                            format(username, password, database))
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
