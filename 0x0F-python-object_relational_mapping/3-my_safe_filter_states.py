74d04bd9c85b057aaa659cc7ccce25e691707d71
ZwaneEM <zwaneem68@outlook.com>
2024-03-17 14:10:10 +0200

    all

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+takes an argument and displays all values in the
+states table of the database where name matches the argument
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ command line arguments """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    nameSearch = argv[4]
+
+    """ Establish the connection to database """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    """ Cursor creation """
+    cursor = conn.cursor()
+
+    sql_query = "SELECT * FROM states WHERE states.name \
+    LIKE %s ORDER BY states.id ASC"
+    cursor.execute(sql_query, (nameSearch,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
