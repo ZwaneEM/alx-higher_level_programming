1393d53599dd39ee77ed82c3c814c16caa45d1f4
ZwaneEM <zwaneem68@outlook.com>
2024-03-18 12:07:37 +0200

    all

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,15 +16,14 @@ if __name__ == "__main__":
     database = argv[3]
 
     engine = create_engine("mysql://{}:{}@localhost:3306/{}".
-                           format(username, password, database))
+                           format(username, password, database), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(State, City).\
-        join(City, State.id == City.state_id).order_by(City.id).all()
+    cities = session.query(City, State).filter(State.id == City.state_id).order_by(City.id).all()
 
     for state, city in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,13 +6,18 @@ Definition of a city
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
+    """
+    Template of the city table
+    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
